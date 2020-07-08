82109d4a57542bc8ffbf1afdd3014a440e5d0451
FlavioWAB <59736888+FlavioWAB@users.noreply.github.com>
2020-07-08 11:53:45 -0300

    Production build optimization

--- webpack.production.js
+++ webpack.production.js
@@ -1,6 +1,23 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CompressionPlugin = require('compression-webpack-plugin');
+const BrotliPlugin = require('brotli-webpack-plugin');
 
-module.exports = merge(common,{
+module.exports = merge(common, {
     mode: "production",
+    plugins: [
+        new CompressionPlugin({ //gzip plugin
+            filename: '[path].gz[query]',
+            algorithm: 'gzip',
+            test: /\.(js|css|html|svg)$/,
+            threshold: 8192,
+            minRatio: 0.8
+        }),
+        new BrotliPlugin({ //brotli plugin
+            asset: '[path].br[query]',
+            test: /\.(js|css|html|svg)$/,
+            threshold: 10240,
+            minRatio: 0.8
+        })
+    ]
 })
